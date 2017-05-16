eb4ebd2cca5a39466c2a3ed07a91292000a04155
Vitor Savedra <vitor-savedra@outlook.com>
2017-05-16 18:40:15 -0300

    Estudos sobre escopos de variáveis.

--- escopoVariaveis.php
+++ escopoVariaveis.php
@@ -1,3 +1,19 @@
 <?php
 
-    
+    // Escopo global. 
+    $nome = "Vitor";
+
+        function teste(){
+            // Escopo local. 
+            $nome = "Savedra";
+            echo $nome;
+        }
+
+        function teste2(){
+            // Usando variável de escopo global. 
+            global $nome;
+            echo $nome;
+        }
+
+        teste();
+        teste2();
