5ecfb180d67da6c97a88a238d457787ee22cca09
Vitor Savedra <vitor-savedra@outlook.com>
2017-05-18 16:34:55 -0300

    Estudos sobre métodos mágicos.

--- metodosMagicos.php
+++ metodosMagicos.php
@@ -1,3 +1,28 @@
 <?php
 
+    class Endereco {
+        
+        private $logradouro;
+        private $numero;
+        private $cidade;
+
+        public function __construct($a, $b, $c) { // Não é necessário que os argumentos tenham o mesmo nome das variáveis.
+            $this->logradouro = $a;
+            $this->numero = $b;
+            $this->cidade = $c;
+        }
+
+        public function __destruct() {
+            var_dump("Destruir");
+        }
+
+        public function __toString() {
+            return $this->logradouro . ", " . $this->numero . ", " . $this->cidade;
+        }
+
+    }
     
+    $meuEndereco = new Endereco("Rua","999","São Paulo");
+    var_dump($meuEndereco);
+    echo "<br><br>" . $meuEndereco . "<br><br>";
+    unset($meuEndereco);
